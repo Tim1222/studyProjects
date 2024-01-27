5f0657243e1b0001c223ad7539efc6c3e12cb957
Tim1222 <timofeu47@gmail.com>
2024-01-27 21:25:50 +0200

    study navlink params

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,24 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Page
-                        1</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Page
-                        2</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Page
-                        3</NavLink></S.NavWrapper>
+                    <S.NavWrapper>
+                        <NavLink to={PATH.PAGE1}
+                                 target={'_blank'}
+                                 rel={'noreferrer nofollow noopener'}
+                        >Page 1</NavLink>
+                        {/*   target={'_blank'} - открывает страницу в новой вкладке
+                        rel - это используется для безопасности, чтобы нельзя было отследить откуда пришел,
+                        не было доступа к сессии*/}
+                    </S.NavWrapper>
+                    <S.NavWrapper>
+                        <NavLink to={PATH.PAGE2}>Page 2</NavLink>
+                    </S.NavWrapper>
+                    <S.NavWrapper>
+                        <NavLink to={PATH.PAGE3} style={(params) => {
+                            return {color: params.isActive ? 'lime' : 'black'}
+                        }
+                        }>Page 3</NavLink>
+                    </S.NavWrapper>
                 </div>
                 <div className={styles.content}>
 
@@ -37,8 +49,11 @@ function App() {
                         <Route path={PATH.PAGE2} element={<PageTwo/>}/>
                         <Route path={PATH.PAGE3} element={<PageThree/>}/>
 
+                        {/*<Route path={PATH.ERROR} element={<Error404/>}/>*/}
+                        {/*<Route path='/*' element={<Navigate to={'/error404'}/>}/>*/}
                         <Route path={PATH.ERROR} element={<Error404/>}/>
-                        <Route path='/*' element={<Navigate to={'/error404'}/>}/>
+                        <Route path='/*' element={<div>Error404</div>}/>
+
                     </Routes>
 
                 </div>
